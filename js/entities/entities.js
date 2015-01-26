4a33044cf5a5a1343681d0f1b72d6c0cded73eff
Miguel Garcia <miguelgarcia2515@gmail.com>
2015-01-26 10:20:07 -0800

    Mr. Baez fixed my error ayyyyy lol

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,11 +1,11 @@
-game.PlayerEntity = me.Entity.({
-	init: function (x, y ,settings) {
+game.PlayerEntity = me.Entity.extend({
+	init: function(x, y ,settings) {
 		this._super(me.Entity, 'init', [x, y, {
 			image: "player",
-			width:64,
-			height :64,
+			width: 64,
+			height: 64,
 			spritewidth: "64",
-			spriteheight "64",
+			spriteheight: "64",
 			getShape : function() {
 				return(new me.Rect(0, 0, 64, 64)).toPolygon() ;
 			}
@@ -13,9 +13,9 @@ game.PlayerEntity = me.Entity.({
 		this.body.setVelocity(5,0);
 
 	},
-	update: function () {
+	update: function(delta) {
 		if(me.input.isKeyPressed("right")){
-			this.body.vel.x += this.body,accel.x = .me.timer.tick;
+			this.body.vel.x += this.body.accel.x = me.timer.tick;
 		}
 		this.body.update(delta);
 		return true;
